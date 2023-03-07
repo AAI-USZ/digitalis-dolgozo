d2f9173ac58a66c5656dd7b27019df3b9698f0e6
vcs <vcs@licquid.com>
2023-03-07 20:54:50 +0100

    NER on issues file

--- src/NER-base.py
+++ src/NER-base.py
@@ -1,11 +1,15 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import pipeline
+import csv
 
 tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
 model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
 
 nlp = pipeline("ner", model=model, tokenizer=tokenizer)
-example = "My name is Wolfgang and I live in Berlin"
 
-ner_results = nlp(example)
-print(ner_results)
+with open('./data/eslint-eslint-issues.csv', 'r', encoding='UTF-8') as issues_csv, \
+     open('./results.txt', 'w', encoding='UTF-8') as result_file:
+    reader = csv.reader(issues_csv, delimiter=',')
+    for row in reader:
+        ner_results = nlp(row[2][2:-1]) #title
+        result_file.write(ner_results + '\n')
