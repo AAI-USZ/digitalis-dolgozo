6c6bb25d98800f70498d5abc704c11752b5250f1
csuvikv <csuvikv@inf.u-szeged.hu>
2023-03-08 09:36:42 +0100

    NER results files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# digitalis-dolgozo
+# Digitális dolgozó
+
+
+- O - Outside of a named entity 
+- B-MIS - Beginning of a miscellaneous entity right  after another miscellaneous entity
+- I-MIS	- Miscellaneous entity
+- B-PER	- Beginning of a person’s name right after another person’s name
+- I-PER	- Person’s name
+- B-ORG	- Beginning of an organization right after another organization
+- I-ORG	- organization
+- B-LOC	- Beginning of a location right after another location
+- I-LOC	- Location
--- src/NER-base.py
+++ src/NER-base.py
@@ -8,8 +8,8 @@ model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
 nlp = pipeline("ner", model=model, tokenizer=tokenizer)
 
 with open('./data/eslint-eslint-issues.csv', 'r', encoding='UTF-8') as issues_csv, \
-     open('./results.txt', 'w', encoding='UTF-8') as result_file:
+     open('results_body.txt', 'w', encoding='UTF-8') as result_file:
     reader = csv.reader(issues_csv, delimiter=',')
     for row in reader:
-        ner_results = nlp(row[2][2:-1]) #title
-        result_file.write(ner_results + '\n')
+        ner_results = nlp(row[3][2:-1])
+        result_file.write(str(ner_results) + '\n')
